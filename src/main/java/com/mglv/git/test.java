72c97507a89b7f7cb26130581f03fbee9d9152f8
MGLV <solitadecho@163.com>
2022-11-06 19:16:54 +0800

    the third commit

--- src/main/java/com/mglv/git/test.java
+++ src/main/java/com/mglv/git/test.java
@@ -11,5 +11,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("this is a test content");
         System.out.println("this is the second content");
+        System.out.println("this is the third content");
     }
 }
