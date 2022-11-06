04777b9b51377c7721d78ac74ea610becf22a108
MGLV <solitadecho@163.com>
2022-11-06 22:12:13 +0800

    the tenth commit

--- src/main/java/com/mglv/git/test.java
+++ src/main/java/com/mglv/git/test.java
@@ -10,10 +10,8 @@ package com.mglv.git;
 public class test {
     public static void main(String[] args) {
         System.out.println("this is a test content");
-        System.out.println("this is the second content");
-        System.out.println("this is the third content");
         System.out.println("this is the fourth content");
-        System.out.println("what's wrong ");
+        System.out.println("what's wrong! ");
         System.out.println("push test. ");
     }
 }
